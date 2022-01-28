ef7cfc9b47f7e6fbd1c7adf6b1407c3d4c7e978b
René Steinhäuser <rene.steinhaeuser@gmx.de>
2022-01-28 17:55:04 +0100

    broken our homepage into smaller components, finished styling our menu-item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,8 @@
+import './homepage.styles.scss'
+import { Directory } from '../../components/directory/directory.component';
+
+export const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
